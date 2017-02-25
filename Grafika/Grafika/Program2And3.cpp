fdecd82792b5bd7ecb5f5cd8378e31b5d812db0c
LukaszJuroszek <lukjuroszek@gmail.com>
2017-02-25 21:02:57 +0100

    Seprate programs 1, 2 and 3 with home work and 4

--- Grafika/Grafika/Program2And3.cpp
+++ Grafika/Grafika/Program2And3.cpp
@@ -1,3 +1,4 @@
+//£ukasz Juroszek inf sem 4, gr 1A 
 #define _USE_MATH_DEFINES
 #include <cmath>
 #include <iostream>;
@@ -12,8 +13,8 @@ GLfloat y = 150.0f;
 //size of polyogon figure
 GLsizei polygonRadiusSize = 50;
 // Rozmiar kroku (liczba pikseli) w osi x i y
-// number of sides of polygon
-int numberOfSides =9;
+// number of sides of polygon ( pentagon )
+int numberOfSides = 5;
 GLfloat xstep = 1.0f;
 GLfloat ystep = 1.0f;
 //window size
@@ -43,11 +44,10 @@ float max(float * inArray) {
 		{
 			max = inArray[i];
 		}
-		
 	}
 	return max;
 }
-// Wywo³ywana w celu przerysowania sceny
+//for program from 1 to 3;
 void RenderScene() {
 	glClear(GL_COLOR_BUFFER_BIT);
 	glColor3f(1.0f, 0.0f, 0.0f);
@@ -61,10 +61,10 @@ void RenderScene() {
 		//cout << round(xx) << ","<< round(yy) <<"  " <<i<< endl;
 		glVertex2f(xx, yy);
 	}
-	
 	glEnd();
 	glutSwapBuffers();
 }
+// ( without homework ) TODO: correct colision 
 void TimerFunction(int value) {
 	float minFromX = min(helperX); // left
 	float maxFromX = max(helperX); // rigth
@@ -73,14 +73,53 @@ void TimerFunction(int value) {
 	//ystep = 0;
 	if (x > windowWidth - polygonRadiusSize || x < polygonRadiusSize)
 		xstep = -xstep;
-	if (y > windowHeight - polygonRadiusSize || y < polygonRadiusSize-minFromY)
+	if (y > windowHeight - polygonRadiusSize || y < polygonRadiusSize - minFromY)
 		ystep = -ystep;
 	/*if (y > windowHeight - polygonRadiusSize || (minFromY-polygonRadiusSize) <0)
 		ystep = -ystep;*/
 	x += xstep;
 	y += ystep;
+	glutReshapeWindow(width,heigth); // change window size to (width,heigth)
 	glutPostRedisplay();
 	glutTimerFunc(10, TimerFunction, 1);
+}
+ //for program 3* TODO: correct colision  (homework)
+void TimerFunctionHomework(int value) {
+	float minFromX = min(helperX); // left
+	float maxFromX = max(helperX); // rigth
+	float minFromY = min(helperY); // bottom
+	float maxFromY = max(helperY); // top
+	bool leftRigthCollision = x > windowWidth - polygonRadiusSize || x < polygonRadiusSize;
+	bool topBottomCollision = y > windowHeight - polygonRadiusSize || y < polygonRadiusSize - minFromY;
+	if (y > windowHeight - polygonRadiusSize )
+	{
+		ystep = 0;
+	}
+	if (x > windowWidth - polygonRadiusSize)
+	{
+		xstep = 0;
+		ystep = -1;
+	}
+	if (y < polygonRadiusSize - minFromY && x > windowWidth - polygonRadiusSize)
+	{
+		ystep = 0;
+		xstep = -1;
+	}
+	if (x < polygonRadiusSize &&y < polygonRadiusSize - minFromY)
+	{
+		xstep = 0;
+		ystep = 1;
+	}
+	if (x < polygonRadiusSize &&y > windowHeight - polygonRadiusSize)
+	{
+		ystep = 0;
+		xstep = 1;
+	}
+	x += xstep;
+	y += ystep;
+	//glutReshapeWindow(width, heigth); // change window size to (width,heigth)
+	glutPostRedisplay();
+	glutTimerFunc(10, TimerFunctionHomework, 1);
 }
 
 void SetupRC() {
@@ -88,16 +127,11 @@ void SetupRC() {
 	glClearColor(0.6f, 0.4f, 0.12f, 1.0f);
 }
 void ChangeSize(GLsizei w, GLsizei h) {
-	// Zabezpieczenie przed dzieleniem przez zero
 	if (h == 0)
 		h = 1;
-	// Ustalenie wielkoœci widoku zgodnego z rozmiarami okna
 	glViewport(0, 0, w, h);
-	// Ustalenie uk³adu wspó³rzêdnych
 	glMatrixMode(GL_PROJECTION);
 	glLoadIdentity();
-	// Keep the square square, this time, save calculated
-	// width and height for later use
 	if (w <= h)
 	{
 		windowHeight = 250.0f*h / w;
@@ -108,24 +142,12 @@ void ChangeSize(GLsizei w, GLsizei h) {
 		windowWidth = 250.0f*w / h;
 		windowHeight = 250.0f;
 	}
-	// Ustanowienie przestrzeni ograniczaj¹cej (lewo, prawo, dó³, góra, blisko, daleko)
 	glOrtho(0.0f, windowWidth, 0.0f, windowHeight, 1.0f, -1.0f);
 	glMatrixMode(GL_MODELVIEW);
 	glLoadIdentity();
 }
-void program1and2(int argc, char*argv[]) {
 
-	glutInit(&argc, argv);
-	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB);
-	glutInitWindowSize(width, heigth);
-	glutCreateWindow("Mój pierwszy program w GLUT");
-	glutDisplayFunc(RenderScene);
-	glutReshapeFunc(ChangeSize);
-	glutTimerFunc(10, TimerFunction, 1);
-	SetupRC();
-	glutMainLoop();
-}
-void program3(int argc, char*argv[]) {
+void program2And3(int argc, char*argv[]) {
 	glutInit(&argc, argv);
 	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB);
 	glutInitWindowSize(width, heigth);
@@ -137,17 +159,21 @@ void program3(int argc, char*argv[]) {
 	glutMainLoop();
 	delete helperX;
 	delete helperY;
-
+}void program3HomeWork(int argc, char*argv[]) {
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB);
+	glutInitWindowSize(width, heigth);
+	glutCreateWindow("Mój pierwszy program w GLUT");
+	glutDisplayFunc(RenderScene);
+	glutReshapeFunc(ChangeSize);
+	glutTimerFunc(10, TimerFunctionHomework, 1);
+	SetupRC();
+	glutMainLoop();
+	delete helperX;
+	delete helperY;
 }
 void main(int argc, char* argv[]) {
-	program3(argc, argv);
-	//glutInit(&argc, argv);
-	//glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB);
-	//glutInitWindowSize(800, 600);
-	//glutCreateWindow("Bounce");
-	//glutDisplayFunc(RenderScene);
-	//glutReshapeFunc(ChangeSize);
-	//glutTimerFunc(33, TimerFunction, 1);
-	//SetupRC();
-	//glutMainLoop();
+	//program2And3(argc, argv);
+	//program3HomeWork(argc, argv);
+
 }
